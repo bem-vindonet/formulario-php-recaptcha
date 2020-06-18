4ffaf8ec6465ce17613ae3522612d33d21e14c62
bem-vindonet <67127634+bem-vindonet@users.noreply.github.com>
2020-06-18 19:51:27 -0300

    Upload do arquivo do formulário

---
+++ contato.php
@@ -0,0 +1,148 @@
+<?php
+/**** Script desenvolvido pelo bem-vindo.net, 2020 ****/
+
+// informar as chaves do ReCaptcha abaixo:
+$chave_de_site = 'INSIRA-AQUI-A-CHAVE-DO-SITE';
+$chave_secreta = 'INSIRA-AQUI-A-CHAVE-SECRETA';
+
+// definir
+$destinatario = 'seuemail@seudominio.com.br';
+$remetente = 'formulario@seudominio.com.br';
+$assunto = 'Contato pelo site';
+$redirecionar_para = '/obrigado.html';
+
+if (isset($_POST['enviar']))
+{
+	// incluir a funcionalidade do recaptcha
+	require_once('recaptchalib.php');
+
+	$erros = [];
+	if (empty($_POST['nome']))
+		$erros[] = 'Nome não preenchido';
+
+	if (empty($_POST['email']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL))
+		$erros[] = 'E-mail não preenchido ou inválido';
+
+	if (empty($_POST['cidade']))
+		$erros[] = 'Cidade não preenchida';
+
+	if (empty($_POST['assunto']))
+		$erros[] = 'Assunto não informado';
+
+	if (empty($_POST['mensagem']))
+		$erros[] = 'Mensagem não fornecida';
+
+	// verificar a chave secreta
+	$response = null;
+	$reCaptcha = new ReCaptcha($chave_secreta);
+
+	if ($_POST['g-recaptcha-response'])
+		$response = $reCaptcha->verifyResponse($_SERVER['REMOTE_ADDR'], $_POST['g-recaptcha-response']);
+
+	if ($response == null || !$response->success)
+		$erros[] = 'Erro na verificação do Recaptcha';
+
+	if (!$erros)
+	{
+		$ip = $_SERVER['REMOTE_ADDR'];
+		$reverso = gethostbyaddr($ip);
+		if ($reverso == $ip)
+			$origem = $ip;
+		else
+			$origem = "$ip ($reverso)";
+		$de = "\"$_POST[nome]\" <$_POST[email]>";
+
+		$corpo = "Origem: $origem
+Navegador: $_SERVER[HTTP_USER_AGENT]
+
+De: $de
+Cidade: $_POST[cidade]
+Assunto: $_POST[assunto]
+
+Mensagem:
+$_POST[mensagem]";
+
+		$headers = "From: $remetente\n";
+		$headers .= "Reply-To: $de";
+
+		if (mail($destinatario, $assunto, $corpo, $headers, "-f$remetente"))
+		{
+			header("Location: $redirecionar_para");
+			exit;
+		}
+		else
+			$erros[] = 'Erro ao mandar seu e-mail, por favor tente novamente mais tarde';
+	}
+}
+
+?>
+<!doctype html>
+<html lang="pt-br">
+<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+	<script src="https://www.google.com/recaptcha/api.js?hl=pt-BR"></script>
+
+	<title>Formulário de contato</title>
+</head>
+<body>
+	<div class="container">
+		<h1>Formulário de contato</h1>
+
+	<?php
+if (!empty($erros))
+{
+?>
+		<div class="alert alert-danger" role="alert">
+			Seu formulário não foi enviado:
+			<ul class="mb-0">
+<?php
+	foreach ($erros as $erro)
+		echo '<li>' . htmlspecialchars($erro) . "</li>\n";
+?>
+			</ul>
+		</div>
+<?php
+}
+?>
+		<form method="POST">
+			<div class="form-group">
+				<label for="nome">Seu nome</label>
+				<input type="text" class="form-control" id="nome" name="nome" value="<?php echo htmlspecialchars($_POST['nome']); ?>" required>
+			</div>
+			<div class="form-group">
+				<label for="email">E-mail</label>
+				<input type="email" class="form-control" id="email" name="email" value="<?php echo htmlspecialchars($_POST['email']); ?>" required>
+			</div>
+			<div class="form-group">
+				<label for="cidade">Cidade</label>
+				<input type="text" class="form-control" id="cidade" name="cidade" value="<?php echo htmlspecialchars($_POST['cidade']); ?>" required>
+			</div>
+			<div class="form-group">
+				<label for="assunto">Assunto</label>
+				<input type="text" class="form-control" id="assunto" name="assunto" value="<?php echo htmlspecialchars($_POST['assunto']); ?>" required>
+			</div>
+			<div class="form-group">
+				<label for="mensagem">Mensagem</label>
+				<textarea class="form-control" id="mensagem" rows="5" name="mensagem" required><?php echo htmlspecialchars($_POST['mensagem']); ?></textarea>
+			</div>
+			<div class="form-group">
+				<div class="g-recaptcha" data-sitekey="<?php echo $chave_de_site; ?>"></div>
+			</div>
+			<button type="submit" class="btn btn-primary" name="enviar" value="Enviar">Enviar</button>
+		</form>
+	</div>
+
+	<!-- Optional JavaScript -->
+	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+	<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+</body>
+</html>
